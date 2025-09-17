3da23dee9649fcc47bc80b9bcb2752ed66d75e75
VHPAIVA <paiva.vhp@gmail.com>
2025-09-16 21:00:07 -0300

    Repo Orgnization
    - Repository Folder Structure
    - Release WorkFlow

---
+++ build/clean.js
@@ -0,0 +1,13 @@
+import path from "path";
+import fs from "fs";
+
+// Clean output directory, or create build directory
+const outDir = path.resolve(process.cwd(), "dist");
+if (fs.existsSync(outDir)) {
+	const filesToClean = fs.readdirSync(outDir).map((dirName) => path.resolve(outDir, dirName));
+	for (const file of filesToClean) {
+		fs.rmSync(file, { recursive: true });
+	}
+} else {
+	fs.mkdirSync(outDir);
+}
---
+++ rollup.config.mjs
@@ -0,0 +1,30 @@
+import copy from '@guanghechen/rollup-plugin-copy';
+
+const staticFiles = [
+	'assets',
+	'fonts',
+	'lang',
+	'templates',
+	'templatesV13',
+	'system.json',
+	// 'LICENCE'
+];
+
+export default [{
+	input: "./src/skyfall.mjs",
+	output: {
+		file: "./dist/skyfall.mjs",
+		format: "es",
+		sourcemap: true
+	},
+	plugins: [
+		copy({
+			targets: [
+				{
+					src: staticFiles.map((f) => `static/${f}`),
+					dest: 'dist',
+				},
+			]
+		})
+	]
+}];
